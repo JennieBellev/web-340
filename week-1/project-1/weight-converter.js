4f680a340e0132d5da53a000d73e226095a38b96
Jennifer Snyder <JennieBellev-jennie.snyder@outlook.com>
2026-03-29 19:54:32 -0500

    Assignment 1.3 CLI complete

--- week-1/project-1/weight-converter.js
+++ week-1/project-1/weight-converter.js
@@ -1,10 +1,26 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Jennifer Snyder
+ * Date: 03/29/2026
+ * File Name: weight-converter.js
+ * Description: Week 1 Chapter 1 Project 1 - Weight Converter
 */
 
 "use strict";
 
-// TODO: Implement the weight conversion logic here
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.error("Usage: node weight-converter.js <pounds>");
+  process.exit(1);
+}
+
+const pounds = Number(args[0]);
+
+if (isNaN(pounds)) {
+  console.error("Input must be a number.");
+  process.exit(1);
+}
+
+const kilograms = pounds * 0.453592;
+
+console.log(kilograms.toFixed(2));
