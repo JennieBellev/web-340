a524eee7e8d44ec44458ad8c495cdd4bc13141c8
Jennifer Snyder <JennieBellev-jennie.snyder@outlook.com>
2026-03-31 22:33:43 -0500

    Week 2 modules assignment complete

--- week-2/project-2/recipes.js
+++ week-2/project-2/recipes.js
@@ -1,23 +1,30 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Jennifer Snyder
+ * Date: 04/01/2026
+ * File Name: recipes.js
+ * Description: This file contains functions for managing recipes
 */
+
+"use strict";
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+return `Recipe created with ingredients: ${ingredients.join(", ")}`;
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
-  // TODO: Implement this function
+  return `Timer set for ${minutes} minutes`;
 }
 
 // Define the quit function
 function quit() {
-  // TODO: Implement this function
+  return "Program exited";
 }
 
-// TODO: Export the functions
+//Export the functions
+module.exports = { 
+  createRecipe,
+  setTimer,
+  quit
+};
