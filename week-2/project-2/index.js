8ac8e198187c3dbb05a7e50e147a33ee4bcd2ed8
Jennifer Snyder <JennieBellev-jennie.snyder@outlook.com>
2026-04-12 22:34:42 -0500

    Fixed file paths and completed week 3 project

--- week-2/project-2/index.js
+++ week-2/project-2/index.js
@@ -1,10 +1,18 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Jennifer Snyder
+ * Date: 04/01/2026
+ * File Name: index.js
+ * Description: This file contains the main entry point for the CLI program
 */
 
+"use strict";
+
+const { createRecipe, setTimer, quit } = require("./recipes");  
+
 // TODO: Import your module using require
+console.log(createRecipe(["flour", "sugar", "eggs"])); 
+console.log(setTimer(30));
+console.log(quit());
+
 
 // TODO: Implement your CLI program here
